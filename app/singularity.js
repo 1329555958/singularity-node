11d779e84bb4f9a1881958fbe1871115f23d4826
1329555958@qq.com <1329555958@qq.com>
2016-12-29 17:10:37 +0800

    'v1'

--- app/singularity.js
+++ app/singularity.js
@@ -18,19 +18,18 @@ var SRequest = require('./model/request');
 
 
 var commandPrams = process.argv.splice(2);
-//console.log(commandPrams);
+console.log(commandPrams);
 var params = UTIL.commandLineParamsToJSON(commandPrams);
 UTIL.moveProperties(params, 'resources', ['cpus', 'memoryMb', 'numPorts']);
-
-//console.log(params);
-var params = {
-    id: "nodejs",
-    owners: "1329555958@qq.com",
-    uris: "hdfs://10.5.16.14:9000/mesos/mesos-2.0.0.jar",
-    command: "java -jar -Dserver.port=$PORT0 -Dserver.context-path=/nodejs mesos-2.0.0.jar",
-    healthcheckUri: "/nodejs/hello",
-    serviceBasePath: "/nodejs",
-    loadBalancerGroups: "testGroup"
-};
+console.log(params);
+//var params = {
+//    id: "nodejs",
+//    owners: "1329555958@qq.com",
+//    uris: "hdfs://10.5.16.14:9000/mesos/mesos-2.0.0.jar",
+//    command: "java -jar -Dserver.port=$PORT0 -Dserver.context-path=/nodejs mesos-2.0.0.jar",
+//    healthcheckUri: "/nodejs/hello",
+//    serviceBasePath: "/nodejs",
+//    loadBalancerGroups: "testGroup"
+//};
 SRequest.createRequest(params);
 
