3b90b4c7eb16a31cae6dee1dcba35b865b3244e2
1329555958@qq.com <1329555958@qq.com>
2017-03-06 17:32:02 +0800

    jdk6 使用

--- dockerInit.sh
+++ dockerInit.sh
@@ -2,6 +2,10 @@
 . /etc/profile
 git clone git://git.vfinance.cn/docker_tools.git /opt/applications/docker_tools
 
+# replace finlog jdk 6
+#rm -f /opt/applications/docker_tools/docker_tools/finlog/finlog.jar
+#mv /opt/finlog-jdk6-*.jar /opt/applications/docker_tools/docker_tools/finlog/finlog.jar
+
 echo before_cmd=$BEFORE_CMD
 /bin/bash -c "$BEFORE_CMD"
 
