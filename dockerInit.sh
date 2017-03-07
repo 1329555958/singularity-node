3dcb76ead191ac2dee8e975a2fd5d4356f56ace8
1329555958@qq.com <1329555958@qq.com>
2017-03-07 09:44:20 +0800

    让日志文件在singularity中也可以看到

--- dockerInit.sh
+++ dockerInit.sh
@@ -6,6 +6,14 @@ git clone git://git.vfinance.cn/docker_tools.git /opt/applications/docker_tools
 #rm -f /opt/applications/docker_tools/docker_tools/finlog/finlog.jar
 #mv /opt/finlog-jdk6-*.jar /opt/applications/docker_tools/docker_tools/finlog/finlog.jar
 
+# logs 让日志文件在singularity中也可以看到
+mkdir -p $MESOS_SANDBOX/opt/logs
+mkdir -p $MESOS_SANDBOX/tomcat/logs
+rm -rf /opt/app/tomcat/logs
+ln -s $MESOS_SANDBOX/opt/logs /opt/logs
+ln -s $MESOS_SANDBOX/tomcat/logs /opt/app/tomcat/logs
+
+
 echo before_cmd=$BEFORE_CMD
 /bin/bash -c "$BEFORE_CMD"
 
